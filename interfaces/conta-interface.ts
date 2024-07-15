a503e779d7b2d4593410738ab626a83ce8eaa757
Ren Wro <134458911+RenWro@users.noreply.github.com>
2024-07-15 20:14:52 -0300

    Update and rename interfaces/conta-interface.ts to src/interfaces/ContaInterface.ts

--- interfaces/conta-interface.ts
+++
@@ -1,7 +0,0 @@
-export interface ContaInterface {
-    numero: number;
-    saldo: number;
-    depositar(valor: number): void;
-    sacar(valor: number): void;
-    transferir(valor: number, contaDestino: ContaInterface): void;
-}
